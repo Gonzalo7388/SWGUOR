7c91e0186642bb2c7c864aaaff7770ed8533d5c8
GieremyBl <daniel_9402@outlook.com>
2026-04-07 15:03:25 -0500

    Se cargo el avance de Josefina v.1.0

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  datasource: {
+datasource: {
     url: process.env.DATABASE_URL,
+    // @ts-ignore: 'directUrl' no está en los tipos actuales pero es necesario para la conexión
     directUrl: process.env.DIRECT_URL,
   },
 })
