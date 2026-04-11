0017412d9c3aaa633d95e414f29c1b7e9c39a4a1
GieremyBl <daniel_9402@outlook.com>
2026-04-11 01:00:50 -0500

    Actualización del schema.prisma

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-datasource: {
-    url: process.env.DATABASE_URL,
-    // @ts-ignore: 'directUrl' no está en los tipos actuales pero es necesario para la conexión
-    directUrl: process.env.DIRECT_URL,
-  },
-})
